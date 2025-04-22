b30d3ec98015f938eac18d9355abdecf35c1fd6d
Huseynov <sahibhuseynov41@gmail.com>
2025-04-23 01:21:28 +0400

    refactor: update image URL for the second slide in Carousel component

--- src/components/dashboard/Slider.jsx
+++ src/components/dashboard/Slider.jsx
@@ -54,7 +54,7 @@ const Carousel = () => {
         "Uşaqlar üçün nəzərdə tutulmuş bu maraqlı riyaziyyat və məntiq tapşırıqları ilə bacarıqlarınızı sınayın!",
     },
     {
-      image: "https://res.cloudinary.com/dwvmtepwh/image/upload/v1745008754/dzubtxs8c22gqm1djypy.webp",
+      image: "https://res.cloudinary.com/dwvmtepwh/image/upload/v1745354589/tgzw6lawq9xuw4wzoucu.png",
       text: "Şir Balası ilə Macəraya Hazır Ol!",
       text2:
         "Ağıllı və bacarıqlı uşaqlar üçün misilsiz tapşırıqlar. Öyrən, əylən və zirvəyə yüksəl!",
